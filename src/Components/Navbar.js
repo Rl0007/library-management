ad8d13ab029510f0b4de76fd4993b45a4167e077
Rahul <12agrawalrahul@gmail.com>
2022-03-07 17:49:24 +0530

    try8

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,14 +6,14 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Library Management</a>
+    <a className="navbar-brand" href="/addbook">Library Management</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-        <Link to={`/`} style={{ textDecoration: 'none' }}>   <a className="nav-link active" aria-current="page" >Book</a></Link>
+        <Link to={`/addbook`} style={{ textDecoration: 'none' }}>   <a className="nav-link active" aria-current="page" >Book</a></Link>
         </li>
         <Link to={`/member`}style={{ textDecoration: 'none' }}>
         <li className="nav-item">
